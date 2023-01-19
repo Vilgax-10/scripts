f6b8ff20fead4cd3cc7ce2d929c3de58740e7866
Vilgax-10 <red98dragon@gmail.com>
2023-01-19 10:29:47 +0000

    Initial Commit

---
+++ getall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir "/bin/linux69"
+cd /bin/linux69
+
+export ImpDir=$(pwd)
+
+mkdir "CC"
+cd CC
+curl "https://raw.githubusercontent.com/Sayyed-Salman/Semester6/main/scripts/dorime.ps1" -o sus.ps1
+cd ..
+
+mkdir "WSN"
+cd WSN
+cd ..
+ 
+mkdir "IR"
+cd IR
+cd ..
+
+mkdir "DS"
+cd DS
+cd ..
